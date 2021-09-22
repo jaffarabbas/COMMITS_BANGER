7dfd62e5a6674e54385418fedf8b4d965b8f8a69
jaffarabbas <fa19bsse0008@maju.edu.pk>
2021-09-22 12:28:50 +0500

    9-22-2021

--- TestProject/CheckingFilesForGit/test2.py
+++ TestProject/CheckingFilesForGit/test2.py
@@ -1,8 +1,14 @@
 import os
+from git import Repo, GitDB
 
 
 def main():
-    print(os.listdir('J:\Program\Github\COMMITS_BANGER\TestProject'))
+    # print(os.listdir('J:\Program\Github\COMMITS_BANGER\TestProject'))
+    #os.system('cmd /c "git status"')
+    repo = Repo("J:\Program\Github\COMMITS_BANGER\TestProject", odbt=GitDB)
+    diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
+    print(diff)
+
 
 
 if __name__ == '__main__':
