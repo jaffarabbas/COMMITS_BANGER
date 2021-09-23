b57366984bb3602079bd1fab0500f3319a1bb151
jaffarabbas <fa19bsse0008@maju.edu.pk>
2021-09-23 18:40:02 +0500

    9-23-2021

---
+++ TestProject/CheckingFilesForGit/version_1.py
@@ -0,0 +1,37 @@
+import os
+from git import Git, Repo, GitDB
+from git.db import GitCmdObjectDB
+from os import path
+
+# from dotenv import load_dotenv
+
+# load_dotenv()
+ListForCommit = []
+url = "J:\C-sharp-Practice"
+url2 = "J:\Program\Github\COMMITS_BANGER"
+repo = Repo(url2, odbt=GitCmdObjectDB)
+diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
+changed = [item.a_path for item in repo.index.diff(None)]
+
+
+def PopulateListWithFiles():
+    for i in repo.untracked_files:
+        ListForCommit.append(i)
+    for i in changed:
+        ListForCommit.append(i)
+    print(ListForCommit)
+
+
+def main():
+    # populate files
+    PopulateListWithFiles()
+    # add and commit the files
+    for i in ListForCommit:
+        repo.index.add(i)
+        print(i)
+        repo.commit(i)
+        print(True)
+
+
+if __name__ == '__main__':
+    main()
