f688a839469e399294f0ca6d78e2efcc6e41a011
jaffarabbas <fa19bsse0008@maju.edu.pk>
2021-09-23 22:49:00 +0500

    23/09/2021 22:48:59

--- TestProject/CheckingFilesForGit/version_1.py
+++ TestProject/CheckingFilesForGit/version_1.py
@@ -10,17 +10,24 @@ origin = repo.remote('origin')
 
 
 def main():
+    #counter for counter commited files
+    count = 0
     # commit message
     commit_message = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     # add files to stage and commit
     for i in repo.untracked_files:
         repo.index.add(i)
         repo.index.commit(commit_message)
+        count += 1
+        print(f'commited file : {i}')
     for i in changed:
         repo.index.add(i)
         repo.index.commit(commit_message)
+        count += 1
+        print(f'commited file : {i}')
+    #push all commits at once
     origin.push()
-
+    print(f"Done !!! commiting : {count} files")
 
 if __name__ == '__main__':
    main()
